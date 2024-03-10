513b5a5f1364e6ffd2e3c36753c4eebb3e382388
jonduvnjak <jon.duvnjak@volumental.com>
2024-03-10 18:26:59 +0100

    name change

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ fn main() -> eframe::Result<()> {
         ..Default::default()
     };
     eframe::run_native(
-        "eframe template",
+        "Jon's WIP template",,
         native_options,
         Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
     )
